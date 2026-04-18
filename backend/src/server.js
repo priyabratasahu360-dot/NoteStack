a02b1b94bae2954203a671b97cea8d39ffc9bdbe
Priyabrat Sahu <priyabratasahu360@gmail.com>
2026-04-18 19:34:44 +0530

    configured project for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
@@ -11,6 +12,7 @@ import noteRoutes from "./routes/note.route.js";
 import userRoutes from "./routes/user.route.js";
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -24,6 +26,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/note", noteRoutes);
 app.use("/api/user", userRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use((req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 app.listen(PORT, (req, res) => {
     console.log(`Server is running port ${PORT}`);
